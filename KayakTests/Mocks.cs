5ffef565024a6f111da15a5c25a33ea142a8d00f
Benjamin van der Veen <b@bvanderveen.com>
2010-09-24 02:21:07 -0700

    oops, forgot to add.

---
+++ KayakTests/Mocks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using Kayak;
+
+namespace KayakTests
+{
+    class Mocks
+    {
+        public static Mock<ISocket> MockSocketRead(IEnumerable<ArraySegment<byte>> chunks)
+        {
+            IEnumerator<ArraySegment<byte>> cs = null;
+
+            var mockSocket = new Mock<ISocket>();
+            mockSocket
+                .Setup(s => s.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>()))
+                .Returns<byte[], int, int>((byte[] b, int o, int c) =>
+                {
+                    if (cs == null)
+                        cs = chunks.GetEnumerator();
+
+                    return Observable.Create<int>(ob =>
+                    {
+                        if (!cs.MoveNext())
+                        {
+                            ob.OnNext(0);
+                            ob.OnCompleted();
+                        }
+
+                        var chunk = cs.Current;
+                        Buffer.BlockCopy(chunk.Array, chunk.Offset, b, o, chunk.Count);
+                        ob.OnNext(chunk.Count);
+                        ob.OnCompleted();
+
+                        return () => { cs.Dispose(); };
+                    });
+                });
+
+            return mockSocket;
+        }
+    }
+}
