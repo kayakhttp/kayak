6d64da24c5b1b18c21da889e1e7c76153b03a6d8
Benjamin van der Veen <bvanderveen@bvanderveen.local>
2011-01-01 04:27:18 -0800

    remove old mock

--- KayakTests/Mocks.cs
+++ KayakTests/Mocks.cs
@@ -38,39 +38,5 @@ namespace KayakTests
 
             return mockSocket;
         }
-
-        //public static Mock<ISocket> MockSocketRead(IEnumerable<ArraySegment<byte>> chunks)
-        //{
-        //    IEnumerator<ArraySegment<byte>> cs = null;
-
-        //    var mockSocket = new Mock<ISocket>();
-        //    mockSocket
-        //        .Setup(s => s.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>()))
-        //        .Returns<byte[], int, int>((byte[] b, int o, int c) =>
-        //        {
-        //            if (cs == null)
-        //                cs = chunks.GetEnumerator();
-
-        //            return Observable.Create<int>(ob =>
-        //            {
-        //                if (!cs.MoveNext())
-        //                {
-        //                    ob.OnNext(0);
-        //                    ob.OnCompleted();
-        //                }
-        //                else
-        //                {
-        //                    var chunk = cs.Current;
-        //                    Buffer.BlockCopy(chunk.Array, chunk.Offset, b, o, chunk.Count);
-        //                    ob.OnNext(chunk.Count);
-        //                    ob.OnCompleted();
-        //                }
-
-        //                return () => { cs.Dispose(); };
-        //            });
-        //        });
-
-        //    return mockSocket;
-        //}
     }
 }
