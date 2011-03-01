35d1708e58fa51df1a21fcd3a157279882836f55
Benjamin van der Veen <bvanderveen@Benjamin-van-der-Veens-MacBook-Pro.local>
2011-03-01 15:48:05 -0800

    added kayak.oars

---
+++ Kayak.Oars/OarsBuffer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kayak.Oars
+{
+    interface IOarsBuffer
+    {
+        void Add(ArraySegment<byte> data);
+        ArraySegment<byte> Remove();
+
+        void Read(IntPtr fd, int count);
+        void Write(IntPtr fd);
+    }
+}
---
+++ Kayak.Oars/OarsServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Oars.Core;
+
+namespace Kayak.Oars
+{
+    public class OarsServer : IServer
+    {
+        public short Backlog;
+        public IServerDelegate Delegate { get; set; }
+        public IPEndPoint ListenEndPoint { get; private set; }
+
+        EventBase eventBase;
+        EVConnListener listener;
+        int connections;
+        bool closed;
+
+        public OarsServer(EventBase eventBase)
+        {
+            this.eventBase = eventBase;
+        }
+
+
+        public void Listen(IPEndPoint ep)
+        {
+            listener = new EVConnListener(eventBase, ep, Backlog);
+            listener.Enable();
+            listener.ConnectionAccepted = (fd, remoteEp) =>
+                {
+                    connections++;
+                    var ev = new EVEvent(eventBase, fd, Events.EV_WRITE | Events.EV_READ);
+                    Delegate.OnConnection(this, new OarsSocket2(ev, remoteEp, this));
+                };
+        }
+
+        public void Close()
+        {
+            closed = true;
+            listener.Dispose();
+
+            if (connections == 0)
+                Delegate.OnClose(this);
+        }
+
+        void SocketClosed()
+        {
+            connections--;
+            if (closed && connections == 0)
+            {
+                Delegate.OnClose(this);
+            }
+        }
+    }
+}
